eea3a688b4b03003f3f68d6d5b93051e6d0fc2e9
.ffd <krekerbruh@hotmail.com>
2025-11-20 12:29:31 +0300

    v1.1

--- src/Task.java
+++ src/Task.java
@@ -1,9 +1,12 @@
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Task {
     private JPanel taskMemory[];
     private int taskCount = 0;
-    Task(int size){
+    //private JFrame activeTask;
+
+    Task(JFrame mainFrame, int size){
         taskMemory = new JPanel[size];
     }
     Task(){
@@ -17,12 +20,19 @@ public class Task {
         System.out.println("Task added, current task count: " + taskCount);
     }
     public void removeTask(int index){
-        taskMemory[index > 0 ? --taskCount : index] = null;
+        if (index > 0){
+            taskMemory[index] = null;
+        }
+        else{
+            taskMemory[taskCount] = null;
+        }
+        taskCount--;
     }
     public void removeTask(){
         if (taskCount > 0) {
             taskMemory[--taskCount] = null;
         }
+        taskCount--;
     }
     public JPanel getTask(int index){
         if (index >= 0 && index < taskCount) {
@@ -39,15 +49,8 @@ public class Task {
     public int getTaskCount() {
         return taskCount;
     }
-    public void setConditionTask(int index, boolean condition){
-        if (index >= 0 && index < taskCount) {
-            taskMemory[index].setVisible(condition);
-        }
+    public void goBack(){
+        taskCount--;
     }
-    public void setConditionTask(boolean condition){
-        if (taskCount >= 0 && taskCount < taskCount) {
-            taskMemory[taskCount].setVisible(condition);
-        }
-    }  
 }
 
